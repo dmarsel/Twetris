2e18f7fc4bb2c7f02891d8f33e204e1d47fdc014
Ilgiz Mustafin <ilgimustafin@gmail.com>
2017-01-11 16:51:17 +0300

    Добавить слушатели мыши в канву

--- src/Canvas.java
+++ src/Canvas.java
@@ -4,12 +4,17 @@
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 import javax.swing.*;
 
-public class Canvas extends JComponent{
+public class Canvas extends JComponent implements MouseListener, MouseMotionListener{
 
-
+	public Canvas() {
+		addMouseListener(this);
+		addMouseMotionListener(this);
+	}
 
     void drawOn(Shape[] s,Graphics g){
         for (int i = 0; i < s.length; i++) {
@@ -71,6 +76,47 @@ public class Canvas extends JComponent{
 
 
     }
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.err.println("На канве тыкнули в точку (" + e.getX() + ", " + e.getY() + ")");
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 
 
