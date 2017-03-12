671795ae81f57fa777d506fd8237eccee044628c
dmarsel <dmarsel@mail.ru>
2017-03-13 00:41:23 +0300

    Не работает
    Добавил метод contains. Он почему-то не может проверить список фигур.
    
    Попробова вынести фигуры paintComponent. Теперь они не добавляются к
    списку

--- src/Canvas.java
+++ src/Canvas.java
@@ -10,6 +10,11 @@ import java.util.ArrayList;
 import javax.swing.*;
 
 public class Canvas extends JComponent implements MouseListener, MouseMotionListener{
+    private int xPosition, yPosition;
+    private boolean drag;
+    public  int activeShapeNumber = -1;
+
+
 
 	public Canvas() {
 		addMouseListener(this);
@@ -28,95 +33,118 @@ public class Canvas extends JComponent implements MouseListener, MouseMotionList
         }
     }
 
+    public  void contains(ArrayList<Shape> s,Point p){
+        for (int i = 0; i < s.size(); i++) {
+            Shape t = s.get(i);
+            if(t.contains(p)){
+                System.out.println("Да");
+                activeShapeNumber=i;}
+        }
+
+    }
+
+    //Фигуры:
+
+    Triangle tr = new Triangle(0, 185, 130);
+    //tr.drawOn(g);
+
+    // g2d.setPaint(Color.RED);
+    Square s = new Square(3, 150, 150);
+    //s.drawOn(g);
+
+    /* 	Устанавливает цвет рисования в зелёный*/
+    // g2d.setPaint(Color.GREEN);
+    Diamond d = new Diamond(0, 150, 150);
+    Diamond d1 = new Diamond(1, 150, 150);
+    Diamond d2 = new Diamond(2, 150, 150);
+
+
+   // Shape[] sh = {tr, s, d, d1, d2};
+//        drawOn(sh,g);
+
+    public static ArrayList<Shape> ShapeList = new ArrayList<>();
+
+    //Добавим фигуры к списку
+        ShapeList.add(tr);
+        ShapeList.add(s);
+        ShapeList.add(d);
+        ShapeList.add(new Diamond(1, 150, 150));
+
+
+
+
+
+
+
 
     /*Метод, перерисовывающий элемент внутри окна
      *при обновлении*/
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponents(g);
-        Graphics2D g2d=(Graphics2D)g;
-        g.drawLine(20, 20, 360, 20);
+        Graphics2D g2d = (Graphics2D) g;
 
+        // g.drawLine(20, 20, 360, 20);
 
 
         g2d.setColor(Color.blue);
 
-        Triangle tr = new Triangle(0,185,130);
-        //tr.drawOn(g);
 
-       // g2d.setPaint(Color.RED);
-        Square s = new Square(3,150,150);
-        //s.drawOn(g);
 
-/* 	Устанавливает цвет рисования в зелёный*/
-       // g2d.setPaint(Color.GREEN);
-        Diamond d = new Diamond(0,150,150);
-        Diamond d1 = new Diamond(1,150,150);
-        Diamond d2 = new Diamond(2,150,150);
-        //d.drawOn(g);
 
-        Shape[] sh={tr,s,d,d1,d2};
-//        drawOn(sh,g);
 
-        ArrayList<Shape> ShapeList = new ArrayList<>();
-        ShapeList.add(tr);
-        ShapeList.add(s);
-        ShapeList.add(d);
-        ShapeList.add(new Diamond(1,150,150));
-        drawOn(ShapeList,g);
 
-     /* int[] arrayX = {20, 100, 100, 250, 250, 20, 20, 200};
-        int[] arrayY = {180, 180, 200, 200, 220, 200, 200, 200};
-        Polygon poly = new Polygon(arrayX, arrayY, 8);
-        g.drawPolygon(poly);
-*/
+
+        drawOn(ShapeList, g);
 
 
 /* 	Вызывает обновление себя после завершения рисования	*/
         super.repaint();
-
-
     }
 
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+        @Override
+        public void mouseDragged (MouseEvent e){
+            // TODO Auto-generated method stub
 
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+        }
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-	}
+        @Override
+        public void mouseMoved (MouseEvent e){
+            // TODO Auto-generated method stub
 
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		System.err.println("На канве тыкнули в точку (" + e.getX() + ", " + e.getY() + ")");
-	}
+        }
 
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+        @Override
+        public void mouseClicked (MouseEvent e){
+            // TODO Auto-generated method stub
+        }
 
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+        @Override
+        public void mousePressed (MouseEvent e){
+            xPosition = e.getX();
+            yPosition = e.getY();
+            Point p = new Point(xPosition, yPosition);
+            contains(ShapeList, p);
+            System.err.println("На канве тыкнули в точку (" + e.getX() + ", " + e.getY() + "). Она лежит в фигуре" + activeShapeNumber);
+            System.err.println("всего "+ShapeList.size()+"фигур");
+        }
 
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+        @Override
+        public void mouseReleased (MouseEvent e){
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void mouseEntered (MouseEvent e){
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void mouseExited (MouseEvent e){
+            // TODO Auto-generated method stub
+
+        }
 
 
 
--- src/Point.java
+++ src/Point.java
@@ -5,9 +5,9 @@ public class Point extends Vector {
     int x;
     int y;
 
-    public Point(int x, int y) {
-        this.x =  x;
-        this.y =  y;
+    public Point(double x, double y) {
+        this.x =  (int) x;
+        this.y =  (int) y;
     }
 
     public Point() {
